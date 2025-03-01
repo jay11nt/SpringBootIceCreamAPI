6cc38bfe5991c5e7e13203bccb867abdfb354a37
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-01 21:10:44 +0530

    Update ChocoIceCream.java

--- main/Entity/ChocoIceCream.java
+++ main/Entity/ChocoIceCream.java
@@ -1,1 +1,19 @@
+package com.example.icecream.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class ChocoIceCream {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String flavorName = "Choco";
+
+    @ManyToMany(mappedBy = "iceCreams")
+    private Set<Category> categories;
+
+    // Getters and setters
+}
 
