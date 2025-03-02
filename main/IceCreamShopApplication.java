2fc26ee190ac6fddfc5efcb66a01d636a9ea396f
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-02 19:22:43 +0530

    Update IceCreamShopApplication.java

--- main/IceCreamShopApplication.java
+++ main/IceCreamShopApplication.java
@@ -1,1 +1,12 @@
+package com.icecreamshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceCreamShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IceCreamShopApplication.class, args);
+    }
+}
 
