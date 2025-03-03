0fbe7392a706e1f85bf940d58d46637c83ac1ff2
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-03 19:52:31 +0530

    Update CategoryRepository.java

--- main/Repository/CategoryRepository.java
+++ main/Repository/CategoryRepository.java
@@ -1,1 +1,8 @@
+package com.example.icecream.repository;
+
+import com.example.icecream.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
 
