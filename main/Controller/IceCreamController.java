8005daf424b6034122c3c41e076594c29f38cb3b
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-03-03 19:57:53 +0530

    Update IceCreamController.java

--- main/Controller/IceCreamController.java
+++ main/Controller/IceCreamController.java
@@ -1,1 +1,28 @@
+@RestController
+@RequestMapping("/api/icecreams")
+public class IceCreamController {
+
+    @Autowired
+    private IceCreamService iceCreamService;
+
+    @PostMapping
+    public IceCreamDTO addIceCream(@RequestBody IceCreamDTO iceCreamDTO) {
+        return iceCreamService.addIceCream(iceCreamDTO);
+    }
+
+    @GetMapping
+    public List<IceCreamDTO> getAllIceCreams() {
+        return iceCreamService.getAllIceCreams();
+    }
+
+    @GetMapping("/{id}")
+    public IceCreamDTO getIceCreamById(@PathVariable Long id) {
+        return iceCreamService.getIceCreamById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteIceCream(@PathVariable Long id) {
+        iceCreamService.deleteIceCream(id);
+    }
+}
 
